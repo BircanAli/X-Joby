9ea5dca9ffb9ca7abfffc26dad55fd24f498a749
Birdzhan Ali <105841521+BircanAli@users.noreply.github.com>
2023-08-17 18:05:11 +0200

    new routes html icon

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,11 @@ import {
   Landing,
   Login,
   Register,
+  AddJob,
+  Stats,
+  AllJobs,
+  Profile,
+  Admin,
 } from "./pages";
 
 const router = createBrowserRouter([
@@ -26,6 +31,28 @@ const router = createBrowserRouter([
       {
         path: "dashboard",
         element: <DashboardLayout />,
+        children: [
+          {
+            index: true,
+            element: <AddJob />,
+          },
+          {
+            path: "profile",
+            element: <Profile />,
+          },
+          {
+            path: "admin",
+            element: <Admin />,
+          },
+          {
+            path: "all-jobs",
+            element: <AllJobs />,
+          },
+          {
+            path: "stats",
+            element: <Stats />,
+          },
+        ],
       },
     ],
   },
--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -1,6 +1,10 @@
+import { Outlet } from "react-router-dom";
+
 const DashboardLayout = () => {
   return (
-    <div>DashboardLayout</div>
-  )
-}
-export default DashboardLayout
+    <div>
+      <Outlet />
+    </div>
+  );
+};
+export default DashboardLayout;
