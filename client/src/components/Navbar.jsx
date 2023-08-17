f5e769a248123f3181dd8189a43306a93a7834ba
Birdzhan Ali <105841521+BircanAli@users.noreply.github.com>
2023-08-17 18:16:46 +0200

    new components files create

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,6 @@
+import Wrapper from "../assets/wrappers/Navbar";
+
+const Navbar = () => {
+  return <Wrapper>Navbar</Wrapper>;
+};
+export default Navbar;
---
+++ client/src/components/SmallSidebar.jsx
@@ -0,0 +1,6 @@
+import Wrapper from "../assets/wrappers/SmallSidebar";
+
+const SmallSidebar = () => {
+  return <Wrapper>SmallSidebar</Wrapper>;
+};
+export default SmallSidebar;
