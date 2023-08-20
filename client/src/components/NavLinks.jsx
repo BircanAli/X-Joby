c953e035e3c420cbc04529f8699255006d723052
Birdzhan Ali <105841521+BircanAli@users.noreply.github.com>
2023-08-20 11:47:18 +0200

    navLinks component added

---
+++ client/src/components/NavLinks.jsx
@@ -0,0 +1,28 @@
+import { useDashboardContext } from "../pages/DashboardLayout";
+import links from "../utils/links";
+import { NavLink } from "react-router-dom";
+
+const NavLinks = () => {
+  const { toggleSidebar } = useDashboardContext();
+
+  return (
+    <div className="nav-links">
+      {links.map((link) => {
+        const { text, path, icon } = link;
+        return (
+          <NavLink
+            onClick={toggleSidebar}
+            to={path}
+            key={text}
+            className="nav-link"
+            end
+          >
+            <span className="icon">{icon}</span>
+            {text}
+          </NavLink>
+        );
+      })}
+    </div>
+  );
+};
+export default NavLinks;
