5a5af8c2792d7cb18ea884070eff5c543fe22001
BircanAli <bircanali91@gmail.com>
2023-09-05 13:16:54 +0200

    password hashing salting pass to DB

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,11 +1,17 @@
 import User from "../models/UserModels.js";
 import { StatusCodes } from "http-status-codes";
+import bcrypt from "bcryptjs";
+import { hashPassword } from "../utils/passwordUtils.js";
 
 export const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments()) === 0;
   req.body.role = isFirstAccount ? "admin" : "user";
+
+  const hashedPassword = await hashPassword(req.body.password);
+  req.body.password = hashedPassword;
+
   const user = await User.create(req.body);
-  res.status(StatusCodes.CREATED).json({ user });
+  res.status(StatusCodes.CREATED).json({ msg: "user is created" });
 };
 export const login = async (req, res) => {
   res.send("login");
