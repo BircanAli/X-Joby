e5f4a4a0a90a1445a07d4d7551389cf7fb9407ce
BircanAli <bircanali91@gmail.com>
2023-09-27 12:42:06 +0200

    static dist folder run commands added

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ cloudinary.config({
 
 //make public folder accessible
 const __dirname = dirname(fileURLToPath(import.meta.url));
-app.use(express.static(path.resolve(__dirname, "./public")));
+app.use(express.static(path.resolve(__dirname, "./client/dist")));
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
@@ -50,7 +50,7 @@ app.use("/api/v1/users", authenticateUser, userRouter);
 app.use("/api/v1/auth", authRouter);
 
 app.use("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./public", "index.html"));
+  res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
 });
 
 // catch non existing url pages res with errorPage
